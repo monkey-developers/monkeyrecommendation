74d9772a7be0c9b5ffc872b0d17686c8cc0a2d20
iguoliveira <igor.or042@gmail.com>
2022-09-08 20:54:50 -0300

    📦 NEW: Global variables style sass

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -2,16 +2,35 @@
 </script>
 
 <div class="navbar-container">
-    <div>MONKEY RECOMENDATION</div>
-    <div>
-        <div>GITHUB</div>
-    </div>
+  <div class="title">Monkey<span>recomendation</span></div>
+  <div class="made-by">
+    Developed by <a href="https://github.com/iguoliveira" target="_blank"
+      >{"<iguoliveira />"}</a
+    >
+  </div>
 </div>
 
 <style lang="scss">
-    .navbar-container{
-        display: flex;
-        justify-content: space-between;
-        padding: 20px 20px;
+  @import "../../variables.scss";
+  .navbar-container {
+    padding: 20px 20px;
+    color: #f4f4f5;
+    font-family: Rubik;
+
+    & > .title {
+      font-size: 2rem;
+
+      & > span {
+        color: $mainColor;
+      }
     }
+
+    & > .made-by {
+      font-size: 1.25rem;
+      & > a {
+        text-decoration: none;
+        color: $mainColor;
+      }
+    }
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './global.css'
+import './global.scss'
 import App from './App.svelte'
 
 const app = new App({
