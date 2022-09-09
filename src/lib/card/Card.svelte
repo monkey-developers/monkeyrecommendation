3fb93ff817ddece2cb51405c3232da4695ab11a1
iguoliveira <igor.or042@gmail.com>
2022-09-08 23:49:13 -0300

    📦 NEW: Card container list

---
+++ src/lib/card/Card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="card-container">
+  <div>FOTO DA OBRA</div>
+  <div>
+    <div>Nome: {"Chainsaw Man"}</div>
+    <div>Nota: </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .card-container {
+    background-color: pink;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 10px;
+  }
+</style>
--- src/routes/Homepage.svelte
+++ src/routes/Homepage.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
   import Navbar from "../lib/navbar/Navbar.svelte";
+  import Card from "../lib/card/Card.svelte";
 </script>
 
 <div>
   <Navbar />
+  <div class="list-container">
+    <Card />
+  </div>
 </div>
 
-<style>
+<style lang="scss">
+  .list-container {
+    background-color: white;
+    padding: 25px;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 </style>
