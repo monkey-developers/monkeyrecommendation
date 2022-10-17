8ef5d53c346eaf523740eb09648ee89aee3f05d2
iguoliveira <igor.or042@gmail.com>
2022-10-17 16:36:54 -0300

    📦 NEW: Added upload file

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,9 +1,22 @@
 import Router from 'express'
+import multer from 'multer'
 
 const router = Router()
 
-router.post('/recommendations', (req, res) => {
-    return res.status(201).send()
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
 })
+
+const upload = multer({ storage })
+
+router.post('/recommendation-image', upload.single('foto'), (req, res) => {
+    const { nome, site } = req.body;
+    res.json({ nome, site });
+});
 
 export { router }
