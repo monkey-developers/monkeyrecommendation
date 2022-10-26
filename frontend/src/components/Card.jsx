f74bea5370a4f25ed7a4b2814fcfd4740607a26d
iguoliveira <igor.or042@gmail.com>
2022-10-26 11:15:03 -0300

    📦 NEW: Star rating component

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,11 +1,23 @@
 import undefined from "../assets/undefined.jpeg";
+import { Rating } from "react-simple-star-rating";
+import { useState, useEffect } from "react";
 
 export const Card = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
+  useEffect(() => {
+    console.log(rating)
+  })
+  
+
   return (
-    <content>
-      <div>
-        <img src={undefined} />
-      </div>
-    </content>
+    <div>
+      <img src={undefined} />
+      <Rating onClick={handleRating} allowFraction={true} initialValue={2.5} />
+    </div>
   );
 };
