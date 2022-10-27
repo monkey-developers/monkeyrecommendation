48f4613e20411914d7d3d3cdab17d1a8c5ce4f3c
iguoliveira <igor.or042@gmail.com>
2022-10-27 03:10:52 -0300

    👌 IMPROVE: Style in form + homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,15 @@
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav>
       <div>
         <div className="main-name">
-          <span>Monkey</span>
-          <a
-            href="https://github.com/monkey-developers/monkeyrecommendation"
-            target={"_blank"}
-            className="name-link"
-          >
-            recommendation
-          </a>
+          <Link to="/">
+            <span>Monkey</span>
+            <span className="project-name">recommendation</span>
+          </Link>
         </div>
         <div className="secondary-name">
           <span>Made by:</span>
--- frontend/src/routes/Homepage.jsx
+++ frontend/src/routes/Homepage.jsx
@@ -1,28 +1,39 @@
-import "./homepageStyle.scss";
+import "./homepage.scss";
 import { Card } from "../components/Card";
 import { Link } from "react-router-dom";
-import { useQuery } from 'react-query'
-import { fetchRecommendations } from "../fetchers/recommendations"
+import { useQuery } from "react-query";
+import { fetchRecommendations } from "../fetchers/recommendations";
 
 export const Homepage = () => {
-  const { data, isLoading, error } = useQuery(['recommend'], fetchRecommendations)
+  const { data, isLoading, error } = useQuery(
+    ["recommend"],
+    fetchRecommendations
+  );
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>{error}</div>
+    return <div>{error}</div>;
   }
 
   return (
     <section className="homepage-container">
+      <Link to={"/recommend"}>recommend</Link>
       {data.recommends.map((item, index) => {
         return (
-          <div key={index}><Card masterpiece={item.masterpiece} rate={item.rate} description={item.description} category={item.category} author={item.author} /></div>
-        )
+          <div key={index}>
+            <Card
+              masterpiece={item.masterpiece}
+              rate={item.rate}
+              description={item.description}
+              category={item.category}
+              author={item.author}
+            />
+          </div>
+        );
       })}
-      <Link to={'/recommend'}>recommend</Link>
     </section>
-  )
+  );
 };
