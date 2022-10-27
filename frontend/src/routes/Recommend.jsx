a56195c9a3af85a724712ee0d03ead535675af6d
iguoliveira <igor.or042@gmail.com>
2022-10-27 03:54:27 -0300

    👌 IMPROVE: Updated the form

--- frontend/src/routes/Recommend.jsx
+++ frontend/src/routes/Recommend.jsx
@@ -1,55 +1,83 @@
-import './recommend.scss'
+import "./recommend.scss";
 import { Link } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 import { Rating } from "react-simple-star-rating";
-import { useMutation, useQueryClient } from 'react-query'
-import { postRecommendations } from '../fetchers/recommendations'
+import { useMutation, useQueryClient } from "react-query";
+import { postRecommendations } from "../fetchers/recommendations";
 
 export const Recommend = () => {
-    const client = useQueryClient()
-    const mutation = useMutation(postRecommendations, {
-        onSuccess: () => {
-            client.invalidateQueries('recommend')
-        }
-    })
-    const [inputs, setInputs] = useState({
-        masterpiece: "",
-        rate: 0,
-        description: "",
-        category: "",
-        author: "",
-    })
+  const client = useQueryClient();
+  const mutation = useMutation(postRecommendations, {
+    onSuccess: () => {
+      client.invalidateQueries("recommend");
+    },
+  });
+  const [inputs, setInputs] = useState({
+    rate: 0,
+    masterpiece: "",
+    author: "",
+    description: "",
+    category: "Anime",
+  });
 
-    const handleChange = (evt) => {
-        setInputs({
-            ...inputs,
-            [evt.target.name]: evt.target.value
-        })
-    }
+  const handleChange = (evt) => {
+    setInputs({
+      ...inputs,
+      [evt.target.name]: evt.target.value,
+    });
+  };
 
-    const handleRating = (rate) => {
-        setInputs({
-            ...inputs,
-            rate
-        })
-    }
+  const handleRating = (rate) => {
+    setInputs({
+      ...inputs,
+      rate,
+    });
+  };
 
-    const handleSubmit = () => {
-        event.preventDefault()
-        mutation.mutate(inputs)
-    }
+  const handleSubmit = () => {
+    event.preventDefault();
+    mutation.mutate(inputs);
+  };
 
-    return (
-        <article className="register-container">
-            <form onSubmit={handleSubmit}>
-                <input name="masterpiece" type="text" placeholder="Masterpiece" value={inputs.masterpiece} onChange={(evt) => handleChange(evt)} />
-                <Rating onClick={handleRating} allowFraction={true} />
-                <input name="description" type="text" placeholder="Description" value={inputs.description} onChange={(evt) => handleChange(evt)} />
-                <input name="category" type="text" placeholder="Category" value={inputs.category} onChange={(evt) => handleChange(evt)} />
-                <input name="author" type="text" placeholder="Author" value={inputs.author} onChange={(evt) => handleChange(evt)} />
-                <button type='submit'>Create</button>
-            </form>
-            <Link to={'/'}>back</Link>
-        </article>
-    )
-}
+  return (
+    <article className="register-container">
+      <Link to={"/"}>back</Link>
+      <form onSubmit={handleSubmit}>
+        <Rating onClick={handleRating} allowFraction={true} />
+        <input
+          name="masterpiece"
+          type="text"
+          placeholder="Show name"
+          value={inputs.masterpiece}
+          onChange={(evt) => handleChange(evt)}
+          required
+        />
+        <input
+          name="author"
+          type="text"
+          placeholder="Recommended by"
+          value={inputs.author}
+          onChange={(evt) => handleChange(evt)}
+          required
+        />
+        <textarea
+          name="description"
+          type="text"
+          placeholder="Description..."
+          maxLength={255}
+          value={inputs.description}
+          onChange={(evt) => handleChange(evt)}
+          required
+        />
+        <select name="category" onChange={(evt) => handleChange(evt)}>
+          <option value="anime">Anime</option>
+          <option value="manga">Manga</option>
+          <option value="serie">Serie</option>
+          <option value="book">Book</option>
+          <option value="movie">Movie</option>
+        </select>
+        <button type="submit">Create</button>
+      </form>
+    </article>
+  );
+};
