a563a0b2e21f544e621dee4814e430908bd7e257
gustavobaltazar <gustavobaltazar31@gmail.com>
2024-04-08 20:32:53 -0300

    fix: popup position

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -1,20 +1,23 @@
 type Props = {
-    text: string
-    closePopup: any
-}
+  text: string;
+  closePopup: any;
+};
 
 export const Popup = ({ text, closePopup }: Props) => {
-    return(
-        <div className="fixed w-screen h-screen flex items-center justify-center">
-            <div className="bg-gray-900 h-fit w-1/2 p-5 rounded-xl">
-                <div className="flex justify-between pb-2 border-gray-600 border-b">
-                    <div className="text-xl font-bold">Synopsis</div>
-                    <button onClick={closePopup} className="bg-main-color p-1 rounded active:bg-transparent active:border-2 active:border-main-color">Close</button>
-                </div>
-                <div className="pt-2">
-                    {text}
-                </div>
-            </div>
+  return (
+    <div className="fixed w-screen h-screen flex items-center justify-center z-50">
+      <div className="bg-gray-900 h-fit w-1/2 p-5 rounded-xl">
+        <div className="flex justify-between pb-2 border-gray-600 border-b">
+          <div className="text-xl font-bold">Synopsis</div>
+          <button
+            onClick={closePopup}
+            className="bg-main-color p-1 rounded active:bg-transparent active:border-2 active:border-main-color"
+          >
+            Close
+          </button>
         </div>
-    )
-}
+        <div className="pt-2">{text}</div>
+      </div>
+    </div>
+  );
+};
